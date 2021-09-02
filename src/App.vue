eb38198a70bf827e5a59d6c1ab619bef508b7184
skawin <skawin@bpnr.co.kr>
2021-09-02 18:13:19 +0900

    added auth

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Amplify Demo App"/>
-  </div>
+
+    <amplify-authenticator>
+      <!-- The rest of your app code -->
+      <amplify-sign-out></amplify-sign-out>
+      <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Amplify Demo App"/>
+      </div>
+    </amplify-authenticator>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  created() {
+    this.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData;
+    })
+  },
+  data() {
+    return {
+      user: undefined,
+      authState: undefined,
+      unsubscribeAuth: undefined
+    }
+  },
+  beforeUnmount() {
+    this.unsubscribeAuth();
   }
 }
 </script>
